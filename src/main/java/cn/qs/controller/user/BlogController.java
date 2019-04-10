ad8b6a0b22b0bd226cc4d1b98709054cc5247020
qiao_zhi <qiao_liqiang@163.com>
2019-04-10 20:13:21 +0800

    完成票务

--- src/main/java/cn/qs/controller/user/BlogController.java
+++ src/main/java/cn/qs/controller/user/BlogController.java
@@ -1,21 +1,34 @@
 package cn.qs.controller.user;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 import cn.qs.bean.user.Blog;
 import cn.qs.bean.user.User;
 import cn.qs.service.user.BlogService;
+import cn.qs.utils.DefaultValue;
 import cn.qs.utils.JSONResultUtil;
+import cn.qs.utils.ValidateCheck;
 
 @Controller
 @RequestMapping("blog")
@@ -41,6 +54,67 @@ public class BlogController {
 		blog.setBlogblank(username);
 
 		blogService.insert(blog);
+		return JSONResultUtil.ok();
+	}
+
+	@RequestMapping("blog_list")
+	public String blog_list() {
+		return "blog-list";
+	}
+
+	@RequestMapping("getBlogs")
+	@ResponseBody
+	public PageInfo<Blog> getBlogs(@RequestParam Map condition, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+		String username = user.getUsername();
+		condition.put("username", username);
+
+		int pageNum = 1;
+		if (ValidateCheck.isNotNull(MapUtils.getString(condition, "pageNum"))) { // 如果不为空的话改变当前页号
+			pageNum = MapUtils.getInteger(condition, "pageNum");
+		}
+		int pageSize = DefaultValue.PAGE_SIZE;
+		if (ValidateCheck.isNotNull(MapUtils.getString(condition, "pageSize"))) { // 如果不为空的话改变当前页大小
+			pageSize = MapUtils.getInteger(condition, "pageSize");
+		}
+
+		// 开始分页
+		PageHelper.startPage(pageNum, pageSize);
+		List<Blog> blogs = new ArrayList<Blog>();
+		try {
+			blogs = blogService.getBlogs(condition);
+		} catch (Exception e) {
+			logger.error("getBlogs error！", e);
+		}
+		PageInfo<Blog> pageInfo = new PageInfo<Blog>(blogs);
+
+		return pageInfo;
+	}
+
+	@RequestMapping("/getBlogdetail/{blogId}")
+	public String getBlogdetail(ModelMap map, @PathVariable() Integer blogId, HttpServletRequest request) {
+		Blog blog = blogService.getBlogdetail(blogId);
+
+		// 获取当前用户
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+		String username = user.getUsername();
+		String blogUsername = StringUtils.defaultIfBlank(blog.getBlogblank(), "admin");
+
+		if (blogUsername.equals(username)) {
+			map.put("blog", blog);
+		} else {
+			map.put("blog", new Blog());
+		}
+
+		return "blogDetail";
+	}
+
+	@RequestMapping("deleteBlog")
+	@ResponseBody
+	public JSONResultUtil deleteBlog(int id) {
+		blogService.deleteBlog(id);
 		return JSONResultUtil.ok();
 	}
 
--- src/main/java/cn/qs/service/user/BlogService.java
+++ src/main/java/cn/qs/service/user/BlogService.java
@@ -1,7 +1,16 @@
 package cn.qs.service.user;
 
+import java.util.List;
+import java.util.Map;
+
 import cn.qs.bean.user.Blog;
 
 public interface BlogService {
 	int insert(Blog blog);
+
+	List<Blog> getBlogs(Map condition);
+
+	Blog getBlogdetail(Integer blogId);
+
+	void deleteBlog(int id);
 }
